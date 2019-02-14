4cf4f53726db10ce64841ad6661d72a27e2f63d6
majki <michofilip@gmail.com>
2019-02-14 21:52:14 +0100

    numeric methods

--- src/main/scala/core/value/basic/NumericValue.scala
+++ src/main/scala/core/value/basic/NumericValue.scala
@@ -1,5 +1,23 @@
 package core.value.basic
 
 import core.value.Value
+import core.value.basic.ByteValue.NumericToByte
+import core.value.basic.DoubleValue.NumericToDouble
+import core.value.basic.FloatValue.NumericToFloat
+import core.value.basic.IntValue.NumericToInt
+import core.value.basic.LongValue.NumericToLong
+import core.value.basic.ShortValue.NumericToShort
 
-trait NumericValue extends Value with OrderedValue
+trait NumericValue extends Value with OrderedValue {
+    def toByteValue: ByteValue = NumericToByte(this)
+    
+    def toShortValue: ShortValue = NumericToShort(this)
+    
+    def toIntValue: IntValue = NumericToInt(this)
+    
+    def toLongValue: LongValue = NumericToLong(this)
+    
+    def toFloatValue: FloatValue = NumericToFloat(this)
+    
+    def toDoubleValue: DoubleValue = NumericToDouble(this)
+}
