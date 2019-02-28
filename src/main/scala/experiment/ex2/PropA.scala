abfe8619f8ee5e4b067899169b0b88597db4dba6
majki <michofilip@gmail.com>
2019-02-28 22:14:12 +0100

    entity experiment2 - not promising

---
+++ src/main/scala/experiment/ex2/PropA.scala
@@ -0,0 +1,8 @@
+//package experiment.ex2
+//
+//trait PropA extends EntityEX {
+//    override protected type T <: PropA
+//    val a: Int
+//
+//    protected def setA(a: Int): T
+//}
---
+++ src/main/scala/experiment/ex2/PropB.scala
@@ -0,0 +1,8 @@
+//package experiment.ex2
+//
+//trait PropB extends EntityEX {
+//    override protected type T <: PropB
+//    val b: Int
+//
+//    protected def setB(b: Int): T
+//}
