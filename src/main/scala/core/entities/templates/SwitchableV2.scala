8677363a4a4922fe28b75d00dba4bad23df65f02
majki <michofilip@gmail.com>
2019-05-01 21:45:03 +0200

    new Switchable redone

--- src/main/scala/core/entities/templates/SwitchableV2.scala
+++ src/main/scala/core/entities/templates/SwitchableV2.scala
@@ -1,26 +1,36 @@
 package core.entities.templates
 
+import core.entities.Entity
 import core.entities.properties.StateHolder
+import core.entities.templates.SwitchableV2.entityToSwitchable
 import core.parts.state.State.{Off, On, SwitchingOff, SwitchingOn}
 
-abstract class SwitchableV2[T <: SwitchableV2[T]] extends MapEntity[T] with StateHolder[T] {
+import scala.language.implicitConversions
+
+abstract class SwitchableV2 extends MapEntity[Entity] with StateHolder[Entity] {
     
     val switchingOffLength = 1000
     val switchingOnLength = 1000
     
-    def beginSwitchingOff(animationStartTime: Long): T = {
+    def beginSwitchingOff(animationStartTime: Long): SwitchableV2 = {
         setState(SwitchingOff).setAnimationStartTime(animationStartTime)
     }
     
-    def finishSwitchingOff(animationStartTime: Long): T = {
+    def finishSwitchingOff(animationStartTime: Long): SwitchableV2 = {
         setState(Off).setAnimationStartTime(animationStartTime)
     }
     
-    def beginSwitchingOn(animationStartTime: Long): T = {
+    def beginSwitchingOn(animationStartTime: Long): SwitchableV2 = {
         setState(SwitchingOn).setAnimationStartTime(animationStartTime)
     }
     
-    def finishSwitchingOn(animationStartTime: Long): T = {
+    def finishSwitchingOn(animationStartTime: Long): SwitchableV2 = {
         setState(On).setAnimationStartTime(animationStartTime)
+    }
+}
+
+object SwitchableV2 {
+    implicit private def entityToSwitchable(entity: Entity): SwitchableV2 = {
+        entity.asInstanceOf[SwitchableV2]
     }
 }
