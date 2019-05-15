049fdf4da14dd6681dfb93ecf0b84ea3ab10a111
majki <michofilip@gmail.com>
2019-05-15 20:10:29 +0200

    entities fix 2

--- src/main/scala/core/entity2/finals/Static2.scala
+++ src/main/scala/core/entity2/finals/Static2.scala
@@ -17,32 +17,60 @@ class Static2(override val id: Long,
         new Static2(id, position, physicsSelector, animationSelector)
     }
     
-    implicit private def toStatic(entity: Entity2): Static2 = entity.asInstanceOf[Static2]
+    implicit private def toStatic(entity: Entity2): Static2 = {
+        entity.asInstanceOf[Static2]
+    }
     
-    override protected def setPosition(position: Position): Static2 = update(position = position)
+    override protected def setPosition(position: Position): Static2 = {
+        update(position = position)
+    }
     
-    override def enableMovement(): Static2 = super.enableMovement()
+    override def enableMovement(): Static2 = {
+        super.enableMovement()
+    }
     
-    override def disableMovement(): Static2 = super.disableMovement()
+    override def disableMovement(): Static2 = {
+        super.disableMovement()
+    }
     
-    override def enableRotation(): Static2 = super.enableRotation()
+    override def enableRotation(): Static2 = {
+        super.enableRotation()
+    }
     
-    override def disableRotation(): Static2 = super.disableRotation()
+    override def disableRotation(): Static2 = {
+        super.disableRotation()
+    }
     
-    override def moveTo(x: Int, y: Int): Static2 = super.moveTo(x, y)
+    override def moveTo(x: Int, y: Int): Static2 = {
+        super.moveTo(x, y)
+    }
     
-    override def moveBy(dx: Int, dy: Int): Static2 = super.moveBy(dx, dy)
+    override def moveBy(dx: Int, dy: Int): Static2 = {
+        super.moveBy(dx, dy)
+    }
     
-    override def rotateTo(direction: Direction): Static2 = super.rotateTo(direction)
+    override def rotateTo(direction: Direction): Static2 = {
+        super.rotateTo(direction)
+    }
     
-    override def rotate45Clockwise(): Static2 = super.rotate45Clockwise()
+    override def rotate45Clockwise(): Static2 = {
+        super.rotate45Clockwise()
+    }
     
-    override def rotate90Clockwise(): Static2 = super.rotate90Clockwise()
+    override def rotate90Clockwise(): Static2 = {
+        super.rotate90Clockwise()
+    }
     
-    override def rotate45Counterclockwise(): Static2 = super.rotate45Counterclockwise()
+    override def rotate45Counterclockwise(): Static2 = {
+        super.rotate45Counterclockwise()
+    }
     
-    override def rotate90Counterclockwise(): Static2 = super.rotate90Counterclockwise()
+    override def rotate90Counterclockwise(): Static2 = {
+        super.rotate90Counterclockwise()
+    }
     
-    override def rotate180(): Static2 = super.rotate180()
+    override def rotate180(): Static2 = {
+        super.rotate180()
+    }
     
 }
