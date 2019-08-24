93d3ff6f3dcc64303d98cd2e2ddb44f513c08819
majki <michofilip@gmail.com>
2019-08-24 21:55:06 +0200

    xml test 2

--- src/main/scala/XMLTest.scala
+++ src/main/scala/XMLTest.scala
@@ -3,16 +3,24 @@ import com.thoughtworks.xstream.io.xml.DomDriver
 import core.entities.EntityFactory
 import core.parts.timer.Timer
 
+import scala.xml.Elem
+
 
 object XMLTest extends App {
-    val timer: Timer = new Timer().start
-    val entityFactory = new EntityFactory(timer)
-    var ent = entityFactory.create("lever").get
+//    val timer: Timer = new Timer().start
+//    val entityFactory = new EntityFactory(timer)
+//    var ent = entityFactory.create("lever").get
+//
+//    val xstream = new XStream(new DomDriver())
+//    xstream.alias(ent.getClass.getSimpleName, ent.getClass)
+//    val xml = xstream.toXML(ent)
+//    println(xml)
     
-    val xstream = new XStream(new DomDriver())
-    xstream.alias(ent.getClass.getSimpleName, ent.getClass)
-    val xml = xstream.toXML(ent)
-    println(xml)
+    case class Car(id: Int, door: Int) {
+        def toXML: Elem =
+            <Car id={id.toString}/>
+    }
     
+    println(Car(2,4).toXML)
     
 }
