0fdde00b08af48687a3b1aa46a6558d2ab920fd3
majki <michofilip@gmail.com>
2019-08-27 21:54:00 +0200

    minor fixes

--- Core/src/main/scala/core/parts/value/CustomBooleanValue.scala
+++ Core/src/main/scala/core/parts/value/CustomBooleanValue.scala
@@ -7,6 +7,16 @@ import value.BooleanValue
 
 object CustomBooleanValue {
     
+    private val isSolid: Entity => Boolean = {
+        case en: PhysicsHolder => en.physics.solid
+        case _ => false
+    }
+    
+    private val isOpaque: Entity => Boolean = {
+        case en: PhysicsHolder => en.physics.opaque
+        case _ => false
+    }
+    
     final case class GetBooleanValue(entityId: Long, name: String)
                                     (implicit entityRepository: EntityRepository) extends BooleanValue {
         override def get: Option[Boolean] = entityRepository.getById(entityId) match {
@@ -25,12 +35,7 @@ object CustomBooleanValue {
     final case class IsSolidAtCoordinates(value: CoordinatesValue)
                                          (implicit entityRepository: EntityRepository) extends BooleanValue {
         override def get: Option[Boolean] = value.get match {
-            case Some(v) =>
-                val condition: Entity => Boolean = {
-                    case en: PhysicsHolder => en.physics.solid
-                    case _ => false
-                }
-                Some(entityRepository.existsAtCoordinates(v, condition))
+            case Some(v) => Some(entityRepository.existsAtCoordinates(v, isSolid))
             case None => None
         }
     }
@@ -38,12 +43,7 @@ object CustomBooleanValue {
     final case class IsOpaqueAtCoordinates(value: CoordinatesValue)
                                           (implicit entityRepository: EntityRepository) extends BooleanValue {
         override def get: Option[Boolean] = value.get match {
-            case Some(v) =>
-                val condition: Entity => Boolean = {
-                    case en: PhysicsHolder => en.physics.opaque
-                    case _ => false
-                }
-                Some(entityRepository.existsAtCoordinates(v, condition))
+            case Some(v) => Some(entityRepository.existsAtCoordinates(v, isOpaque))
             case None => None
         }
     }
--- Core/src/main/scala/core/repository/EntityRepository.scala
+++ Core/src/main/scala/core/repository/EntityRepository.scala
@@ -14,23 +14,17 @@ class EntityRepository private(private val entitiesById: Map[Long, Entity],
                 val coordinates = en.position.coordinates
                 val newEntitiesAtCoordinates = entitiesByCoordinates.getOrElse(coordinates, Map.empty) + (en.id -> en)
                 entitiesByCoordinates + (coordinates -> newEntitiesAtCoordinates)
-            case _ =>
-                entitiesByCoordinates
+            case _ => entitiesByCoordinates
         }
         new EntityRepository(newEntitiesById, newEntitiesByCoordinates)
     }
     
-    def +(entity: Entity): EntityRepository = {
-        add(entity)
-    }
+    def +(entity: Entity): EntityRepository = add(entity)
     
-    def addAll(entities: Seq[Entity]): EntityRepository = {
+    def addAll(entities: Seq[Entity]): EntityRepository =
         entities.foldLeft(this)((entityHolder, entity) => entityHolder + entity)
-    }
     
-    def ++(entities: Seq[Entity]): EntityRepository = {
-        addAll(entities)
-    }
+    def ++(entities: Seq[Entity]): EntityRepository = addAll(entities)
     
     def remove(entity: Entity): EntityRepository = {
         val newEntitiesById = entitiesById - entity.id
@@ -42,58 +36,39 @@ class EntityRepository private(private val entitiesById: Map[Long, Entity],
                     entitiesByCoordinates - coordinates
                 else
                     entitiesByCoordinates + (coordinates -> newEntitiesAtCoordinates)
-            case _ =>
-                entitiesByCoordinates
+            case _ => entitiesByCoordinates
         }
         new EntityRepository(newEntitiesById, newEntitiesByCoordinates)
     }
     
-    def -(entity: Entity): EntityRepository = {
-        remove(entity)
-    }
+    def -(entity: Entity): EntityRepository = remove(entity)
     
-    def removeAll(entities: Seq[Entity]): EntityRepository = {
+    def removeAll(entities: Seq[Entity]): EntityRepository =
         entities.foldLeft(this)((entityHolder, entity) => entityHolder - entity)
-    }
     
-    def --(entities: Seq[Entity]): EntityRepository = {
-        removeAll(entities)
-    }
+    def --(entities: Seq[Entity]): EntityRepository = removeAll(entities)
     
-    def contains(id: Long): Boolean = {
-        entitiesById.contains(id)
-    }
+    def contains(id: Long): Boolean = entitiesById.contains(id)
     
-    def getAll: Vector[Entity] = {
-        entitiesById.values.toVector
-    }
+    def getAll: Vector[Entity] = entitiesById.values.toVector
     
-    def getById(id: Long): Option[Entity] = {
-        entitiesById.get(id)
-    }
+    def getById(id: Long): Option[Entity] = entitiesById.get(id)
     
-    def getByCoordinates(coordinates: Coordinates): Vector[Entity] = {
+    def getByCoordinates(coordinates: Coordinates): Vector[Entity] =
         entitiesByCoordinates.getOrElse(coordinates, Map.empty).values.toVector
-    }
     
-    def existsAtCoordinates(coordinates: Coordinates, condition: Entity => Boolean): Boolean = {
+    def existsAtCoordinates(coordinates: Coordinates, condition: Entity => Boolean): Boolean =
         getByCoordinates(coordinates).exists(condition)
-    }
     
-    def forallAtCoordinates(coordinates: Coordinates, condition: Entity => Boolean): Boolean = {
+    def forallAtCoordinates(coordinates: Coordinates, condition: Entity => Boolean): Boolean =
         getByCoordinates(coordinates).forall(condition)
-    }
     
 }
 
 object EntityRepository {
-    def apply(): EntityRepository = {
-        new EntityRepository(Map.empty, Map.empty)
-    }
+    def apply(): EntityRepository = new EntityRepository(Map.empty, Map.empty)
     
-    def apply(entities: Seq[Entity]): EntityRepository = {
-        EntityRepository() ++ entities
-    }
+    def apply(entities: Seq[Entity]): EntityRepository = EntityRepository() ++ entities
 }
 
 
